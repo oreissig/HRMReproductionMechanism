e9fbb0a761f934ca50b8325984810ebbc857e3e9
oreissig <oreissig+github@gmail.com>
2015-10-25 20:14:45 +0100

    disable type checking for now

--- src/main/groovy/com/github/oreissig/hrm/backend/interpreter/LabelListener.groovy
+++ src/main/groovy/com/github/oreissig/hrm/backend/interpreter/LabelListener.groovy
@@ -1,8 +1,7 @@
 package com.github.oreissig.hrm.backend.interpreter
 
 import groovy.transform.CompileStatic
-
-import org.antlr.v4.runtime.ParserRuleContext
+import groovy.transform.TypeCheckingMode
 
 import com.github.oreissig.hrm.frontend.parser.HRMBaseListener
 import com.github.oreissig.hrm.frontend.parser.HRMParser
@@ -15,11 +14,13 @@ class LabelListener extends HRMBaseListener {
         this.labels = labels
     }
 
+    // somehow type check fails on TravisCI
+    @CompileStatic(TypeCheckingMode.SKIP)
     @Override
     void enterLabel(HRMParser.LabelContext ctx) {
         def label = ctx.ID().text
         def statement = ctx.parent.parent
-        ParserRuleContext program = statement.parent
+        def program = statement.parent
         def index = program.children.indexOf(statement)
         labels[label] = index
     }
